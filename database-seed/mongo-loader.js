c48e478915d4b48097e3398876828923cd6c328c
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-02-07 20:22:48 -0800

    Promisified the mongo-loader and added drop functionality

--- database-seed/mongo-loader.js
+++ database-seed/mongo-loader.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const DATABASE_LINK = process.env.DATABASE_LINK || require('../config.js').DATABASE_LINK;
 
-mongoose.connect(DATABASE_LINK);
+mongoose.connect(DATABASE_LINK, { useNewUrlParser: true, useCreateIndex: true });
 
 const productImageScheme = mongoose.Schema({
     productID: {
@@ -19,14 +19,37 @@ const productImageScheme = mongoose.Schema({
 let Product = mongoose.model('ProductImage', productImageScheme);
 
 const seedDatabase = () => {
-    let documents = JSON.parse(retrieveData());
-    documents.forEach((document) => {
-        Product.updateOne({'productID':document.productID}, document, {upsert: true}, (err) => {
-            if (err) throw err;
+    dropDatabase()
+    .then(() => {
+        let documents = JSON.parse(retrieveData());
+        let updates = [];
+        documents.forEach((document) => {
+            updates.push(update(document));
+        });
+        Promise.all(updates).then(() => {
+            console.log('Database seeded');
+            process.exit();
         })
     });
-    console.log('seed complete, exiting in 5 seconds');
-    setTimeout(()=>{process.exit()}, 5000);
+}
+
+const update = (document) => {
+    return new Promise((resolve, reject) => {
+        Product.updateOne({'productID':document.productID}, document, {upsert: true}, (err) => {
+            if (err) reject(err);
+            resolve();
+        });
+    });
+}
+
+const dropDatabase = () => {
+    return new Promise((resolve, reject) => {
+        Product.deleteMany({}, (err) => {
+            if (err) reject(err);
+            console.log('Database dropped');
+            resolve();
+        })
+    })
 }
 
 const retrieveData = () => {

3fec9568dadd16adb914775ca1d6e20d23acc5dd
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-02-08 15:26:23 -0800

    Externalized the db schema

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-//***************** SERVER ******************
+//**************************** SERVER *****************************
 const express = require('express');
 const server = express();
 const path = require('path');
@@ -13,9 +13,11 @@ const initialize = () => {
 }
 
 const router = {
+
     serveClient: () => {
         server.use(express.static('../client/dist'));
         server.get('/', (req, res) => {
+            console.log('sending html?');
             let index = path.join(__dirname, '../client/index.html');
             res.sendFile(index, (err) => {
                 if (err) throw err;
@@ -23,32 +25,33 @@ const router = {
             });
         });
     },
+
     allowImageRetrieval: () => {
-        server.get('/index/:productID/', (req, res) => {
+        server.get('/images/:productID/', (req, res) => {
+            console.log('imgage req rcvd');
             let productID = req.params.productID;
             queryDatabase(productID).then((document) => {
-                console.log(document);
-                //res.send(document, () => {
-                // for sending to the client later.
-                // we dont need to do this yet.
-                // res.end();
-                //}
+                res.send(document, () => {
+                    res.statusCode = 200;
+                    res.end();
+                });
             });
         })
     }
 };
 
 
-//*********** DATABASE CONNECTION ***********
+//********************** DATABASE CONNECTION **********************
 const mongoose = require('mongoose');
 const DATABASE_LINK = process.env.DATABASE_LINK || require('../config.js').DATABASE_LINK;
-const DB_SCHEMA = require('../database-seed/mongo-loader.js').DB_SCHEMA;
+const DB_SCHEMA = require('../database-seed/database-schema.js').DB_SCHEMA;
 
 const queryDatabase = (productID) => {
-    mongoose.connection(DATABASE_LINK, { useNewUrlParser: true, useCreateIndex: true });
+    mongoose.connect(DATABASE_LINK, { useNewUrlParser: true, useCreateIndex: true });
     let Product = mongoose.model('ProductImage', DB_SCHEMA);
     return new Promise((resolve, reject) => {
-        Product.findOne({productID: productId}, (err, result) => {
+        Product.findOne({productID: productID}, (err, result) => {
+            mongoose.connection.close();
             if (err) reject(err);
             resolve(result);
         })
@@ -56,5 +59,5 @@ const queryDatabase = (productID) => {
 }
 
 
-
+//********************** START SERVER **********************
 initialize();

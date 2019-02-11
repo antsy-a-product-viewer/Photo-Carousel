b2bf806a2241b27e364ceb85c907d26ffe0f6064
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-02-11 12:58:16 -0800

    Added external CSS styling module

---
+++ client/src/components/styles.css.js
@@ -0,0 +1,20 @@
+export default {
+  thumbnailImage: {
+    width: '40px',
+    height: '40px'
+  },
+  scaledImage: {
+    width: '578px',
+    height: '578px'
+  },
+  thumbnailBar: {
+    display: 'flex',
+    'flexDirection': 'row',
+    'justifyContent': 'center'
+  },
+  container: {
+    width: '580px',
+    height: '640px',
+    backgroundColor: 'grey'
+  }
+};

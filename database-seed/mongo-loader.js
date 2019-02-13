565e011cbdc872707d676d347e158ef5cc716cc7
Jonathan Mitzman <Jonathan.mitzman@gmail.com>
2019-02-13 15:52:43 -0800

    Allowed for random generation of image counts

--- database-seed/mongo-loader.js
+++ database-seed/mongo-loader.js
@@ -9,43 +9,43 @@ mongoose.connect(DATABASE_LINK, { useNewUrlParser: true, useCreateIndex: true })
 let Product = mongoose.model('ProductImage', DB_SCHEMA);
 
 const seedDatabase = () => {
-    dropDatabase()
+  dropDatabase()
     .then(() => {
-        let documents = JSON.parse(retrieveData());
-        let updates = [];
-        documents.forEach((document) => {
-            updates.push(update(document));
-        });
-        Promise.all(updates).then(() => {
-            console.log('Database seeded');
-            mongoose.connection.close();
+      let documents = JSON.parse(retrieveData());
+      let updates = [];
+      documents.forEach((document) => {
+        updates.push(update(document));
+      });
+      Promise.all(updates).then(() => {
+        console.log('Database seeded');
+        mongoose.connection.close();
 
-        })
+      })
     });
-}
+};
 
 const update = (document) => {
-    return new Promise((resolve, reject) => {
-        Product.updateOne({'productID':document.productID}, document, {upsert: true}, (err) => {
-            if (err) reject(err);
-            resolve();
-        });
+  return new Promise((resolve, reject) => {
+    Product.updateOne({'productID': document.productID}, document, {upsert: true}, (err) => {
+      if (err) reject(err);
+      resolve();
     });
-}
+  });
+};
 
 const dropDatabase = () => {
-    return new Promise((resolve, reject) => {
-        Product.deleteMany({}, (err) => {
-            if (err) reject(err);
-            console.log('Database dropped');
-            resolve();
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    Product.deleteMany({}, (err) => {
+      if (err) reject(err);
+      console.log('Database dropped');
+      resolve();
+    });
+  });
+};
 
 const retrieveData = () => {
-    let source = path.join(__dirname,'dummydata.js');
-    return fs.readFileSync(source,'utf-8');
-}
+  let source = path.join(__dirname,'dummydata.js');
+  return fs.readFileSync(source,'utf-8');
+};
 
 seedDatabase();
--- database-seed/mongod-document-generator.js
+++ database-seed/mongod-document-generator.js
@@ -4,39 +4,39 @@ const fs = require('fs');
 const path = require('path');
 
 const rand = () => {
-    return Math.ceil(Math.random()*12);
-}
+  return Math.ceil(Math.random()*12);
+};
 
 const generateDocs = (num) => {
-    let collection = [];
-    for (let i = 1; i <= num; i++) {
-        let newDoc = {};
-        newDoc.productID = i;
-        newDoc.images = createImageArray(i);
-        collection.push(newDoc);
-    }
-    return collection;
-}
+  let collection = [];
+  for (let i = 1; i <= num; i++) {
+    let newDoc = {};
+    newDoc.productID = i;
+    newDoc.images = createImageArray(i);
+    collection.push(newDoc);
+  }
+  return collection;
+};
 
 const createImageArray = (prodID, n = rand()) => {
-    let images = [];
-    for (let i = 0; i < n; i++) {
-        let image = {
-            url: `https://s3-us-west-1.amazonaws.com/beyond-antsy/images/${prodID}/placeholder.jpg`,
-            sort: i
-        };
-        images.push(image);
-    }
-    return images;
-}
+  let images = [];
+  for (let i = 0; i < n; i++) {
+    let image = {
+      url: `https://s3-us-west-1.amazonaws.com/beyond-antsy/images/${prodID}/placeholder.jpg`,
+      sort: i
+    };
+    images.push(image);
+  }
+  return images;
+};
 
 const writeDocs = (docs) => {
-    let data = JSON.stringify(docs);
-    let destination = path.join(__dirname, 'dummydata.js');
-    fs.writeFile(destination, data, 'utf-8', (err) => {
-        if(err) throw err;
-    })
-}
+  let data = JSON.stringify(docs);
+  let destination = path.join(__dirname, 'dummydata.js');
+  fs.writeFile(destination, data, 'utf-8', (err) => {
+    if (err) throw err;
+  });
+};
 
 //uncomment to run:
 writeDocs(generateDocs(100));
